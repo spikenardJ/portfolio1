9025533f40b89c6c717fcbe29ad9a3b8931c9407
spikenardJ <jennifer_annette@hotmail.com>
2025-03-09 18:19:38 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onscroll = onScroll;
+
+function onScroll() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.body.classList.remove("scroll-top");
+  } else {
+    document.body.classList.add("scroll-top");
+  }
+} 
